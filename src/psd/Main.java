65419eece7cd14c8fb445354b0be6f900d66b0ed
Pavel Sytnik <pashasytnik@gmail.com>
2024-02-15 17:52:50 +0400

    Implement data input for Xs

--- src/psd/Main.java
+++ src/psd/Main.java
@@ -2,8 +2,9 @@ package psd;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
+import psd.node.*;
 import psd.token.Token;
 
 public class Main {
@@ -24,5 +25,26 @@ public class Main {
 
         var syntaxAnalyzer = new SyntaxAnalyzer();
         var root = syntaxAnalyzer.createNodes(list);
+        Node currentNode = root;
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            if (currentNode instanceof StartNode sn) {
+                System.out.println("B");
+                currentNode = sn.getNextNode();
+            } else if (currentNode instanceof LabelNode ln) {
+                currentNode = ln.getNextNode();
+            } else if (currentNode instanceof CommandNode cn) {
+                System.out.println("Y" + cn.getIndex());
+                currentNode = cn.getNextNode();
+            } else if (currentNode instanceof ConditionNode cn) {
+                System.out.print("X" + cn.getIndex() + ": ");
+                int x = sc.nextInt();
+                currentNode = x == 0 ? cn.getFalseNode() : cn.getTrueNode();
+            } else if (currentNode instanceof EndNode) {
+                System.out.println("E");
+                break;
+            }
+        }
     }
 }
